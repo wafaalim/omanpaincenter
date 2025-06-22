178c52c8168fa26847fdca08041a91aba0ff7a75
Wafaa LIMOUNA <wlimouna-externe@thekooples.com>
2025-06-22 23:34:21 +0200

    Force redeploy to remove image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -37,7 +37,7 @@ export default function About() {
         <div className="flex flex-col items-center gap-8">
           <div>
             <Image
-              src=""
+              src="/images/Wafaa.PNG"
               alt="CEO Wafaa LIMOUNA"
               width={300}
               height={300}
@@ -47,7 +47,7 @@ export default function About() {
           </div>
           <div>
             <Image
-              src=""
+              src="/images/Yehia.PNG"
               alt="Dr. Yehia EL BENDARY, Neurosurgeon"
               width={300}
               height={300}
